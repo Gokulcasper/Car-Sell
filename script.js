d0518d618aef69d29942e1f159d676b26d05ce8b
Gokulcasper <ronitcasper215@gmail.com>
2022-07-05 19:33:11 +0530

    Feat : Swiper

--- script.js
+++ script.js
@@ -47,3 +47,27 @@ document.querySelector(".home").onmouseleave = () => {
     elm.style.transform = `translateX(0px) translateY(0px)`;
   });
 };
+var swiper = new Swiper(".vehicles-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
