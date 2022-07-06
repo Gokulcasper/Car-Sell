0109767bbb917cb1c4a2eea7030196949eabb36a
Gokulcasper <ronitcasper215@gmail.com>
2022-07-06 12:45:50 +0530

    Feat : Review Section End

--- script.js
+++ script.js
@@ -101,3 +101,30 @@ var swiper = new Swiper(".featured-slider", {
     },
   },
 });
+
+var swiper = new Swiper(".reviews-slider", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    991: {
+      slidesPerView: 3,
+    },
+  },
+});
